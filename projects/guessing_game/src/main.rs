0aa11d57884c9c1922d652d38e1c5f93a107005b
SusanHex <suehex4027@gmail.com>
2022-06-01 09:13:22 -0600

    finished first part of the guessing game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+// Rusts standard input/output
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("What is your guess?: ");
+    // create a mutable String variable
+    let mut guess = String::new();
+    
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("Your guess was: {}", guess)
+
 }
